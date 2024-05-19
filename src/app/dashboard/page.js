96fa11920fe947911df91a38221ef60b85c460f7
akulvarshney <allenv213@gmail.com>
2024-05-19 10:44:05 +0530

    Project Setup

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,5 @@
+//use client;
+
+export default function Dashboard() {
+  return <h1>This is the Dashboard</h1>;
+}
---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <>
+      <h1>404 PAGE</h1>
+      <Link href={"/"}>Go to Home Page</Link>
+    </>
+  );
+}
